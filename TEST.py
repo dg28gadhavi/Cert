054fa7aee843dbb85bf811c6dc8ca969f37e872a
Dhruv Gadhavi <dg28gadhavi@gmail.com>
2021-10-26 11:14:57 +0530

    Make a dir and save the output file in a certain dir

--- TEST.py
+++ TEST.py
@@ -6,21 +6,22 @@ print("Generating Your TEST Certificates......")
 print("Cetificates for", name, "are being Generated.")
 
 #path = 'temp'
-#os.mkdir(path)
+os.mkdir(name)
+os.chdir(name)
 
 class PDF(FPDF):
 
 #Header
     def header(head):
-        head.image('logo.png', 10, 10, 50)
+        head.image('../logo.png', 10, 10, 50)
 
 #Sidebar
-        head.image('side.png', 190, 0, 20)
+        head.image('../side.png', 190, 0, 20)
 
 #Footer
     def footer(foot):
         foot.set_y(-10)
-        foot.image('footer.jpg', 0, 192, 211)
+        foot.image('../footer.jpg', 0, 192, 211)
 
 pdf = PDF('P', 'mm', 'A4')
 
